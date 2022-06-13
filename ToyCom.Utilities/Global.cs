8ead176af42b2b69de006ccc513bc0342b9017d5
y-govor <ygovor3@gmail.com>
2022-06-13 22:49:28 +0300

    Added Global class

---
+++ ToyCom.Utilities/Global.cs
@@ -0,0 +1,10 @@
+﻿namespace ToyCom.Utilities
+{
+    /// <summary>
+    /// Class for storing data that is available globally
+    /// </summary>
+    public static class Global
+    {
+        public static string TextEditorLastText = string.Empty;
+    }
+}
